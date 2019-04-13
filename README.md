61d3b430b3f691a182bd5d38e7d4f10feec82650
Jakub Pawelczak <jakub.pawelczak.pl@gmail.com>
2019-04-13 17:22:39 +0100

    Fixed model and added initial external API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Software Testing Type Capsule
+
+Integration with Wikipedia API
--- code/TestingType.js
+++ code/TestingType.js
@@ -1,12 +1,22 @@
 var console = require("console")
+var http = require('http')
+var config = require('config')
+var fail = require('fail')
+var text = require('./lib/TestingOverviews.js')
 
 module.exports.function = function testingType (testingTypeName) {
-  var text = "Random text to check if it works";
-  console.log(text);
-  console.log(testingTypeName);
+  
+  //testing object downloaded from wikipedia API - link below
+  //the API mocked in TestingOverviews.js
+  console.log(text.query.pages[222828].extract)
+  var testingOverview = text.query.pages[222828].extract;
+  
+  //API works, but dont know how to call parameter of the object
+  var response = http.getUrl('https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&prop=extracts&exintro=&explaintext=&titles=Unit%20testing');
+  console.log(response.batchcomplete);
   
   return {
     testingTypeName: testingTypeName,
-    testingOverview: text
+    testingOverview: testingOverview
   }
 }
