952ef19aa2cb727c85d2a5aa14d3b1e5094cc1ee
Jakub Pawelczak <jakub.pawelczak.pl@gmail.com>
2019-04-19 19:21:14 +0100

    Added de-DE support

--- code/TestingType.js
+++ code/TestingType.js
@@ -3,31 +3,60 @@ var http = require('http')
 //var config = require('config')
 //var text = require('./lib/TestingOverviews.js')
 
-module.exports.function = function testingType (testingTypeName) {
+module.exports.function = function testingType (testingTypeName, $vivContext) {
   
-  //integration with Wikipedia API
-  
+  console.log($vivContext)
+  // Generate the right locale translation
+  var language = $vivContext.locale.split('-')[0];
+  console.log(language);
+    
+  //integration with en.wikipedia.com via its API
   if (testingTypeName == "Unit testing") {
-     var type = "Unit%20testing"
+     
+    if(language == "en") { 
+       var type = "Unit%20testing"
+     } else {
+       var type = "Modultest";
+     }
      
   } else if (testingTypeName == "Integration testing") {
-     var type = "Integration%20testing"    
+     
+     if(language == "en") { 
+       var type = "Integration%20testing"
+     } else {
+       var type = "Integrationstest";
+     }
      
   } else if (testingTypeName == "System testing") {
-     var type = "System%20testing" 
+     
+     if(language == "en") { 
+       var type = "System%20testing"
+     } else {
+       var type = "Systemprüfung";
+     }
      
   } else {
-     var type = "Operational%20acceptance%20testing"
+     
+     if(language == "en") { 
+       var type = "Operational%20acceptance%20testing"
+     } else {
+       var type = "Abnahmeprüfungen";
+     }
+    
   }
     
-  var response = http.getUrl('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + type);
+  var response = http.getUrl('https://' + language + '.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + type);
   
   var obj = JSON.parse(response)
   for(var key in obj.query.pages) {
     var testingOverview = obj.query.pages[key].extract
+    //var testingTypeName = obj.query.pages[key].title
     console.log(testingOverview);
+    //console.log(testingTypeName);
   }
   
+  console.log(obj.query.pages[key].title)
+
   return {
     testingTypeName: testingTypeName,
     testingOverview: testingOverview
